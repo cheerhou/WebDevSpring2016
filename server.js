1939881294cdf5678aeddd56915a440cbb281934
Chenjin <hou.c@husky.neu.edu>
2016-01-15 11:02:52 -0500

    change hello content

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', function(req, res){
-    res.send('hello world');
+    res.send('hello world, from chenjin');
 });
 app.listen(port, ipaddress);
