e2f6e72c0dfb5ad32ac558c89a74a6e34a1fda18
Chenjin <hou.c@husky.neu.edu>
2016-01-15 11:30:49 -0500

    move index page to public dir

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-#!/bin/env node
-//  OpenShift sample Node application
 var express = require('express');
 var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
 app.get('/hello', function(req, res){
     res.send('hello world, from chenjin');
 });
+
 app.listen(port, ipaddress);
