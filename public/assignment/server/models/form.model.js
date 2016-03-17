ca8df07d67298c9fbeda2dfa2d7152eada927149
Chenjin <hou.c@husky.neu.edu>
2016-03-17 17:35:55 -0400

    add API for user and form

--- public/assignment/server/models/form.model.js
+++ public/assignment/server/models/form.model.js
@@ -1,3 +1,63 @@
-/**
- * Created by cheerhou on 3/17/16.
- */
+//use the mock data for testing
+var mockForms = require("./form.mock.json");
+
+module.exports = function(app) {
+    var api = {
+        createForm: createForm,
+        findAllForm: findAllForm,
+        findFormById: findFormById,
+        updateForm: updateForm,
+        deleteForm: deleteForm,
+        findFormByTitle: findFormByTitle
+
+    };
+    return api;
+
+    function createForm(form) {
+        mockForms.push(form);
+        return mockForms;
+    }
+
+    function findAllForm() {
+        return mockForms;
+    }
+
+    function findFormById(formId) {
+        formId = parseInt(formId);
+        for(var i in mockForms) {
+            if(mockForms[i]._id === formId) {
+                return mockForms[i];
+            }
+        }
+        return null;
+    }
+
+    function updateForm(formId, newForm) {
+        formId = parseInt(formId);
+        for(var i in mockForms) {
+            if (mockForms[i]._id === formId) {
+                mockForms[i].title = newForm.title;
+                mockForms[i].userId = newForm.userId;
+                mockForms[i].fields = newForm.fields;
+            }
+        }
+    }
+
+    function deleteForm(formId) {
+        formId = parseInt(formId);
+        for(var i in mockForms) {
+            if (mockForms[i]._id === formId) {
+                mockForms.splice(i, 1);
+            }
+        }
+    }
+
+    function findFormByTitle(title) {
+        for(var i in mockForms) {
+            if (mockForms[i].title === title) {
+                return mockForms[i];
+            }
+        }
+        return null;
+    }
+}
--- public/assignment/server/models/user.model.js
+++ public/assignment/server/models/user.model.js
@@ -1,3 +1,74 @@
-module.exports = function(app) {
+//use the mock data for testing
+var mockUsers = require("./user.mock.json");
 
+module.exports = function(app) {
+    var api = {
+        createUser: createUser,
+        findAllUser: findAllUser,
+        findUserById: findUserById,
+        updateUser: updateUser,
+        deleteUser: deleteUser,
+        findUserByUsername: findUserByUsername,
+        findUserByCredentials: findUserByCredentials
+
+    };
+    return api;
+
+    function createUser(user) {
+        mockUsers.push(user);
+        return mockUsers;
+    }
+
+    function findAllUser() {
+        return mockUsers;
+    }
+
+    function findUserById(userId) {
+        userId = parseInt(userId);
+        for(var i in mockUsers) {
+            if(mockUsers[i]._id === userId) {
+                return mockUsers[i];
+            }
+        }
+        return null;
+    }
+
+    function updateUser(userId, newUser) {
+        userId = parseInt(userId);
+        for(var i in mockUsers) {
+            if (mockUsers[i]._id === userId) {
+                mockUsers[i].firstName = newUser.firstName;
+                mockUsers[i].lastName = newUser.lastName;
+                mockUsers[i].username = newUser.username;
+                mockUsers[i].password = newUser.password;
+            }
+        }
+    }
+
+    function deleteUser(userId) {
+        userId = parseInt(userId);
+        for(var i in mockUsers) {
+            if (mockUsers[i]._id === userId) {
+                mockUsers.splice(i, 1);
+            }
+        }
+    }
+
+    function findUserByUsername(username) {
+        for(var i in mockUsers) {
+            if (mockUsers[i].username === username) {
+                return mockUsers[i];
+            }
+        }
+        return null;
+    }
+
+    function findUserByCredentials(credentials) {
+        for(var i in mockUsers) {
+            if (mockUsers[i].username === credentials.username && mockUsers[i].password === credentials.password) {
+                return mockUsers[i];
+            }
+        }
+        return null;
+    }
 }
