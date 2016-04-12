3fea4e7fd61fc6ca7939f1d955d79f8eb1a44a53
Chenjin <hou.c@husky.neu.edu>
2016-04-11 22:25:57 -0400

    configure server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
-var request = require("request");
 
+// load passport module
+var passport = require('passport');
+
+// load cookie parsers
+var cookieParser = require('cookie-parser');
+
+// load session support
+var session      = require('express-session');
 
 var connectionString = "mongodb://127.0.0.1:27017/cs5610";
 //Openshift mongoDB
@@ -20,11 +27,20 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var session = require('express-session');
+// configure cookie parser - needed for oauth
+app.use(cookieParser());
+
+// configure session support
 app.use(session({ secret: 'meanstackisthebest' }));
 
+// initialize passport and session support
+app.use(passport.initialize());
+app.use(passport.session());
+
+// configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
+//configure local or openShift ip address and port
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
@@ -36,5 +52,3 @@ require("./public/assignment/server/app.js")(app, db, mongoose);
 require("./public/project/server/app.js")(app, db);
 
 app.listen(port, ipaddress);
-
-
