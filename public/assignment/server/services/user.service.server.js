96ef7e25b19cb3c64b4c130b2f67998b8d53f22d
Chenjin <hou.c@husky.neu.edu>
2016-04-15 15:38:25 -0400

    bug fix for update email again

--- public/assignment/server/services/user.service.server.js
+++ public/assignment/server/services/user.service.server.js
@@ -211,43 +211,43 @@ module.exports = function (app, userModel) {
                 }
                 newUser.roles = roles;
             }
-
-            //encrypt the password when password is changed
-            userModel.findUserById(userId)
-                .then(
-                    function (user) {
-                        //when password changed
-                        var oldPassword = user.password;
-                        if (newUser.password != oldPassword) {
-                            newUser.password = bcrypt.hashSync(newUser.password);
-                            //console.log("encrypt when update" + newUser.password);
-                        }
-
-                        return userModel.updateUser(userId, newUser);
-                    }
-                )
-                .then(
-                    function (users) {
-                        res.json(users);
-                    },
-                    function (err) {
-                        res.status(400).send(err);
-                    }
-                );
-
-
-
-                //userModel
-                //    .updateUser(userId, newUser)
-                //    .then(
-                //        function (users) {
-                //            res.json(users);
-                //        },
-                //        function (err) {
-                //            res.status(400).send(err);
-                //        }
-                //    );
         }
+
+        //encrypt the password when password is changed
+        userModel.findUserById(userId)
+            .then(
+                function (user) {
+                    //when password changed
+                    var oldPassword = user.password;
+                    if (newUser.password != oldPassword) {
+                        newUser.password = bcrypt.hashSync(newUser.password);
+                        //console.log("encrypt when update" + newUser.password);
+                    }
+
+                    return userModel.updateUser(userId, newUser);
+                }
+            )
+            .then(
+                function (users) {
+                    res.json(users);
+                },
+                function (err) {
+                    res.status(400).send(err);
+                }
+            );
+
+
+        //userModel
+        //    .updateUser(userId, newUser)
+        //    .then(
+        //        function (users) {
+        //            res.json(users);
+        //        },
+        //        function (err) {
+        //            res.status(400).send(err);
+        //        }
+        //    );
+
     }
 
     function deleteUser(req, res) {
